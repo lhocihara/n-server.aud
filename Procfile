de5a62ffb065f614c6580461bee598e9a3c0abb8
Lucas Hocihara <lucas.hocihara@gmail.com>
2019-09-16 14:45:34 -0300

    alterando

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: python run.py
 worker: python auditor.py
--- run.py
+++ run.py
@@ -6,7 +6,7 @@ envi_prod = True
 # configuração de IP e porta
 # ----------------------------------------------------------
 if __name__ == '__main__' and envi_prod:
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(debug=False, use_reloader=False, host='0.0.0.0', port=port)
 
 elif __name__ == '__main__' and not envi_prod:
