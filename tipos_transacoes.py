e756c18d6e9be681d301e7f10d60b67c9908f046
Lucas Hocihara <lucas.hocihara@gmail.com>
2019-09-22 01:55:52 -0300

    adicionando auditor de crud pessoa + interações externas com dados de pessoa

--- tipos_transacoes.py
+++ tipos_transacoes.py
@@ -4,27 +4,23 @@ class Tipos_Transacoes:
   def __init__(self):
     self.transacoes = []
 
-
   # --------------------------------------------------------------------------
   # Logs acionados por acões em sistemas da aplicação
   # --------------------------------------------------------------------------
   def log_interno_cadastro_inicial_usuario(self, dados, t = ""):
-    
     timestamp_do_evento = t if t != "" else datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
-
     formato_log = {
       "tipo_log": "interno_cadastro_inicial",
-      "id_usuario": dados["id"],
+      "id_usuario": dados["id_usuario"],
       "datetime": timestamp_do_evento
     }
-    
     self.transacoes.append(formato_log)
 
   def log_interno_login_usuario(self, dados, t = ""):
     timestamp_do_evento = t if t != "" else datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
     formato_log = {
       "tipo_log": "login_interno_usuario",
-      "id_usuario": dados["id"],
+      "id_usuario": dados["id_usuario"],
       "datetime": timestamp_do_evento
     }
     self.transacoes.append(formato_log)
@@ -33,7 +29,7 @@ class Tipos_Transacoes:
     timestamp_do_evento = t if t != "" else datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
     formato_log = {
       "tipo_log": "interno_consulta_dados_usuario",
-      "id_usuario": dados["id"],
+      "id_usuario": dados["id_usuario"],
       "datetime": timestamp_do_evento
     }
     self.transacoes.append(formato_log)
@@ -42,7 +38,8 @@ class Tipos_Transacoes:
     timestamp_do_evento = t if t != "" else datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
     formato_log = {
       "tipo_log": "adicao_dados_usuario",
-      "id_usuario": dados["id"],
+      "id_usuario": dados["id_usuario"],
+      "campos_adicionados": dados["campos_adicionados"],
       "datetime": timestamp_do_evento
     }
     self.transacoes.append(formato_log)
@@ -51,7 +48,8 @@ class Tipos_Transacoes:
     timestamp_do_evento = t if t != "" else datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
     formato_log = {
       "tipo_log": "alteracao_dados_usuario",
-      "id_usuario": dados["id"],
+      "id_usuario": dados["id_usuario"],
+      "campos_alterados": dados["campos_alterados"],
       "datetime": timestamp_do_evento
     }
     self.transacoes.append(formato_log)
@@ -60,28 +58,29 @@ class Tipos_Transacoes:
     timestamp_do_evento = t if t != "" else datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
     formato_log = {
       "tipo_log": "exclusao_dados_usuario",
-      "id_usuario": dados["id"],
+      "id_usuario": dados["id_usuario"],
+      "campos_excluidos": dados["campos_excluidos"],
       "datetime": timestamp_do_evento
     } 
     self.transacoes.append(formato_log)
 
   def log_solicitacao_exclusao_usuario(self, dados, t = ""):
-    
     timestamp_do_evento = t if t != "" else datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
-
     formato_log = {
       "tipo_log": "solicitacao_exclusao_usuario",
-      "id_usuario": dados["id"],
+      "id_usuario": dados["id_usuario"],
+      "data_vencimento": dados["data_vencimento"],
       "datetime": timestamp_do_evento
     }
-    
     self.transacoes.append(formato_log)
 
   def log_interno_criacao_vinculo(self, dados, t = ""):
     timestamp_do_evento = t if t != "" else datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
     formato_log = {
-      "tipo_log": "cadastro_inicial",
-      "id_usuario": dados["id"],
+      "tipo_log": "interno_criacao_vinculo",
+      "id_usuario": dados["id_usuario"],
+      "id_projeto": dados["id_projeto"],
+      "id_vinculo": dados["id_vinculo"],
       "datetime": timestamp_do_evento
     }
     self.transacoes.append(formato_log)
@@ -89,8 +88,10 @@ class Tipos_Transacoes:
   def log_interno_exclusao_vinculo(self, dados, t = ""):
     timestamp_do_evento = t if t != "" else datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
     formato_log = {
-      "tipo_log": "cadastro_inicial",
-      "id_usuario": dados["id"],
+      "tipo_log": "interno_exclusao_vinculo",
+      "id_usuario": dados["id_usuario"],
+      "id_projeto": dados["id_projeto"],
+      "id_vinculo": dados["id_vinculo"],
       "datetime": timestamp_do_evento
     }
     self.transacoes.append(formato_log)
@@ -103,7 +104,9 @@ class Tipos_Transacoes:
     timestamp_do_evento = t if t != "" else datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
     formato_log = {
       "tipo_log": "externo_login_usuario",
-      "id_usuario": dados["id"],
+      "id_usuario": dados["id_usuario"],
+      "id_projeto": dados["id_projeto"],
+      "id_vinculo": dados["id_vinculo"],
       "datetime": timestamp_do_evento
     }
     self.transacoes.append(formato_log)
@@ -112,7 +115,10 @@ class Tipos_Transacoes:
     timestamp_do_evento = t if t != "" else datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
     formato_log = {
       "tipo_log": "externo_consulta_dados_usuario",
-      "id_usuario": dados["id"],
+      "id_usuario": dados["id_usuario"],
+      "id_projeto": dados["id_projeto"],
+      "id_vinculo": dados["id_vinculo"],
+      "campos_consultados": dados["campos_consultados"],
       "datetime": timestamp_do_evento
     }
     self.transacoes.append(formato_log)
@@ -121,7 +127,9 @@ class Tipos_Transacoes:
     timestamp_do_evento = t if t != "" else datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
     formato_log = {
       "tipo_log": "externo_criacao_vinculo",
-      "id_usuario": dados["id"],
+      "id_usuario": dados["id_usuario"],
+      "id_projeto": dados["id_projeto"],
+      "id_vinculo": dados["id_vinculo"],
       "datetime": timestamp_do_evento
     }
     self.transacoes.append(formato_log)
@@ -130,7 +138,9 @@ class Tipos_Transacoes:
     timestamp_do_evento = t if t != "" else datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
     formato_log = {
       "tipo_log": "externo_exclusao_vinculo",
-      "id_usuario": dados["id"],
+      "id_usuario": dados["id_usuario"],
+      "id_projeto": dados["id_projeto"],
+      "id_vinculo": dados["id_vinculo"],
       "datetime": timestamp_do_evento
     }
     self.transacoes.append(formato_log)
